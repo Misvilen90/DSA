7287530acf6a046a0c7cbb219835e507dc4b9689
Priyanka T <priyankathiyagu11@gmail.com>
2026-02-03 07:22:32 +0530

    Add Java program to print Pascal's Triangle
    Implement Pascal's Triangle in Java.

---
+++ patterns/Pascal’s Triangle.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        
+        for ( int i = 0 ; i <= 4 ;i++){
+            for (int j = 0 ; j <= 4-i ; j++){
+                System.out.print(" ");
+            }
+            int val = 1;
+            for (int j=0; j<= i ;j++){
+                System.out.print(val+" ");
+                val = val * (i - j) / (j + 1);
+            }
+            
+            System.out.println();
+        }
+       
+    }
+}
