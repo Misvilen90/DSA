11b9e288def8a476a57dc4af29b3df5803e93a3b
Priyanka T <priyankathiyagu11@gmail.com>
2026-02-05 06:12:02 +0530

    Implement max and min calculation for an array
    This program takes an array of integers as input and calculates the maximum and minimum values.

---
+++ Array/Max and Min of Array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of element");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enther the number");
+        for (int i = 0 ; i < n ; i++){
+            arr[i]=sc.nextInt();
+        }
+        int max = 0 , min = arr[0];
+        for(int i =0 ; i < n ;i++){
+            if( arr[i]>max){
+                max = arr[i];
+            }
+            if( arr[i]< min){
+                min = arr[i];
+            }
+        }
+        System.out.println(max+" "+min);
+    }
+}
