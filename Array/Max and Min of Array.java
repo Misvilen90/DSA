15d9c3f3098543cf73c166a7bbcb3ee73ac32b49
Priyanka T <priyankathiyagu11@gmail.com>
2026-02-05 06:14:31 +0530

    Initialize max and min with first array element

--- Array/Max and Min of Array.java
+++ Array/Max and Min of Array.java
@@ -9,7 +9,7 @@ class Main {
         for (int i = 0 ; i < n ; i++){
             arr[i]=sc.nextInt();
         }
-        int max = 0 , min = arr[0];
+        int max = arr[0] , min = arr[0];
         for(int i =0 ; i < n ;i++){
             if( arr[i]>max){
                 max = arr[i];
