4dcf0de448156b3e44307882329f8cd7eca98a1e
Priyanka T <priyankathiyagu11@gmail.com>
2026-03-09 21:16:30 +0530

    Create Clear Bit.java

---
+++ Bit Manipulation/Clear Bit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int pos = s.nextInt();
+        int bitmask = ~(1 << pos) ;
+        System.out.println(bitmask&n);
+    }
+}
